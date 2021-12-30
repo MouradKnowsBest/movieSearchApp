05b44778420b0efe3dc018dcf2e5bb9ef10a67c1
MouradKnowsBest <mourad.ab01@gmail.com>
2021-12-30 14:13:41 +0100

    added budget info to movie slider

--- modules/createMovieDev.js
+++ modules/createMovieDev.js
@@ -1,68 +1,60 @@
-import {IMG_PATH} from './links.js';
+import { IMG_PATH } from "./links.js";
 
-export async function createMovieDev(movieEl, youtubeLink, poster_path, title, vote_average, overview, movieBudget){
+export async function createMovieDev(
+  movieEl,
+  youtubeLink,
+  poster_path,
+  title,
+  vote_average,
+  overview,
+  movieBudget, 
+  release_date
+) {
+  var imgPath = IMG_PATH + poster_path;
 
-    var imgPath = IMG_PATH + poster_path 
+  var lastFour = imgPath.substr(imgPath.length - 4); // => last 4 letters
 
-    var lastFour = imgPath.substr(imgPath.length - 4); // => last 4 letters
- 
-    if (lastFour == "null") { // error handling
-        createMoviesDivWithError(movieEl, youtubeLink, poster_path, title, vote_average, overview, movieBudget)
-    }
+  if (lastFour == "null") {
+    // error handling
+    createMoviesDivWithError(
+      movieEl,
+      youtubeLink,
+      poster_path,
+      title,
+      vote_average,
+      overview,
+      movieBudget,
+      release_date
 
-    else {
-        createMoviesDiv(movieEl, youtubeLink, poster_path, title, vote_average, overview, movieBudget)
-   }
- 
-   }
+    );
+  } else {
+    createMoviesDiv(
+      movieEl,
+      youtubeLink,
+      poster_path,
+      title,
+      vote_average,
+      overview,
+      movieBudget,
+      release_date
 
-   
-function getClassByRate(vote) {
-    if (vote >= 8) {
-        return "green";
-    } else if (vote >= 5) {
-        return "orange";
-    } else {
-        return "red";
-    }
-}
-
-function createMoviesDivWithError(movieEl, youtubeLink, vote_average, overview, movieBudget){
-    movieEl.classList.add("movie");
-
-    movieEl.innerHTML = `
-    <img
-        src=".//images//error_img.jpg"
-        alt=""
-    />
-
-    <div class="movie-info">
-        <h3> ERROR : Sorry Image not found </h3>
-        <span class="${getClassByRate(vote_average)}">
-        ${vote_average}</span>
-    </div>
-    <div class="overview">
-    <a href=${youtubeLink} target ="_blank"> 
-        <h3 style="text-align:center; color:#373b69 ;font-size: 28px ;font-weight: bolder; border: 1px solid #373b69">
-            TRAILER 
-        </h3>
-    </a>
-      <h3>Overview:</h3>
-      ${overview}
-      <h3>Budjet:</h3> <strong class="budget"> ${formatNumber(movieBudget)} $ </strong>
-  </div>
-
-    `;
-
-    main.appendChild(movieEl);
-}
-
-function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    );
   }
+}
 
-function createMoviesDiv(movieEl, youtubeLink, poster_path, title, vote_average, overview, movieBudget) {
-    movieEl.innerHTML = `
+
+function createMoviesDiv(
+  movieEl,
+  youtubeLink,
+  poster_path,
+  title,
+  vote_average,
+  overview,
+  movieBudget,
+  release_date
+
+) {
+  movieEl.innerHTML = `
 
     <img
         src="${IMG_PATH + poster_path}"
@@ -80,9 +72,68 @@ function createMoviesDiv(movieEl, youtubeLink, poster_path, title, vote_average,
           </h3>
       </a>
 
+
         <h3>Overview:</h3>
         ${overview}
-        <h3>Budjet:</h3> <strong class="budget"> ${formatNumber(movieBudget)} $ </strong>
+        <h3>Release Date:</h3> <strong class="release-date">${release_date}</strong>
+
+        <h3>Budjet:</h3> 
+        
+        <strong class="budget"> ${formatNumber(movieBudget)} $ </strong>
+        
         </div>
     `;
+}
+
+function createMoviesDivWithError(
+  movieEl,
+  youtubeLink,
+  vote_average,
+  overview,
+  movieBudget
+) {
+  movieEl.classList.add("movie");
+
+  movieEl.innerHTML = `
+    <img
+        src=".//images//error_img.jpg"
+        alt=""
+    />
+
+    <div class="movie-info">
+        <h3> ERROR : Sorry Image not found </h3>
+        <span class="${getClassByRate(vote_average)}">
+        ${vote_average}</span>
+    </div>
+    <div class="overview">
+    <a href=${youtubeLink} target ="_blank"> 
+        <h3 style="text-align:center; color:#373b69 ;font-size: 28px ;font-weight: bolder; border: 1px solid #373b69">
+            TRAILER 
+        </h3>
+    </a>
+    <h3>Overview:</h3>
+      ${overview}
+      <h3>Release Date:</h3> <strong class="release-date">${release_date}</strong>
+      <h3>Budjet:</h3> <strong class="budget"> ${formatNumber(
+        movieBudget
+      )} $ </strong>
+  </div>
+
+    `;
+
+  main.appendChild(movieEl);
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
